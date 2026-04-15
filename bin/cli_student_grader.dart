471fac647c3fad4dee00f85aafc6783a1211d37b
Musa Kazim <musakazim989@gmail.com>
2026-04-15 21:45:08 +0600

    feat: add title, subjects, students list

--- bin/cli_student_grader.dart
+++ bin/cli_student_grader.dart
@@ -1,5 +1,7 @@
-import 'package:cli_student_grader/cli_student_grader.dart' as cli_student_grader;
+const String appTitle = "Student Grader v1.0";
 
-void main(List<String> arguments) {
-  print('Hello world: ${cli_student_grader.calculate()}!');
-}
+final Set<String> availableSubjects = {'Math', 'Science', 'English'};
+
+List<Map<String, dynamic>> students = [];
+
+void main(List<String> arguments) {}
