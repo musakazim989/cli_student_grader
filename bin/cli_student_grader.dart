6a8d91667c3c8a6ffe27b2158f49708dba8c2356
Musa Kazim <musakazim989@gmail.com>
2026-04-15 21:57:40 +0600

    feat: implement menu loop

--- bin/cli_student_grader.dart
+++ bin/cli_student_grader.dart
@@ -1,7 +1,55 @@
+import 'dart:io';
+
 const String appTitle = "Student Grader v1.0";
 
 final Set<String> availableSubjects = {'Math', 'Science', 'English'};
 
 List<Map<String, dynamic>> students = [];
 
-void main(List<String> arguments) {}
+var shouldExit = false;
+
+const menuOptions =
+    '''
+===== $appTitle =====
+
+1. Add Student
+2. Record Score
+3. Add Bonus Points
+4. Add Comment
+5. View All Students
+6. View Report Card
+7. Class Summary
+8. Exit
+
+Choose an option:
+''';
+
+void main(List<String> arguments) {
+  do {
+    print(menuOptions);
+    String? choice = stdin.readLineSync();
+
+    switch (choice) {
+      case '1':
+        break;
+      case '2':
+        break;
+      case '3':
+        break;
+      case '4':
+        break;
+      case '5':
+        break;
+      case '6':
+        break;
+      case '7':
+        break;
+      case '8':
+        shouldExit = true;
+        print("Exiting $appTitle. Goodbye!");
+        break;
+      default:
+        print("Invalid option. Please try again.");
+    }
+  } while (shouldExit == false);
+}
