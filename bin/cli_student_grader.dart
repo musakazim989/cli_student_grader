40b3479fa0c712e0653daccfcc0c273a6721024c
Musa Kazim <musakazim989@gmail.com>
2026-04-15 22:30:34 +0600

    feat: implement record score for students

--- bin/cli_student_grader.dart
+++ bin/cli_student_grader.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 const String appTitle = "Student Grader v1.0";
 
-final Set<String> availableSubjects = {'Math', 'Science', 'English'};
+final Set<String> availableSubjects = {'Math', 'Science', 'English', 'History'};
 
 List<Map<String, dynamic>> students = [];
 
@@ -43,6 +43,40 @@ void main(List<String> arguments) {
         print("Student '$name' added successfully.");
         break;
       case '2':
+        for (int i = 0; i < students.length; i++) {
+          print("${i + 1}. ${students[i]['name']}");
+        }
+        int? studentIndex;
+        while (true) {
+          print('Select student number to record score:');
+          String? studentChoice = stdin.readLineSync();
+          studentIndex = int.tryParse(studentChoice ?? '') ?? -1;
+
+          if (studentIndex < 1 || studentIndex > students.length) {
+            print("Invalid student number. Please try again.");
+            continue;
+          } else {
+            break;
+          }
+        }
+
+        var subjectList = availableSubjects.toList();
+        for (int i = 0; i < subjectList.length; i++) {
+          while (true) {
+            print('Enter Score for ${subjectList[i]}: ');
+            String? scoreInput = stdin.readLineSync();
+            double score = double.tryParse(scoreInput ?? '') ?? -1;
+            if (score < 0 || score > 100) {
+              print("Invalid score. Please enter a value between 0 and 100.");
+              continue;
+            } else {
+              students[studentIndex - 1]['scores'].add(score);
+              break;
+            }
+          }
+        }
+        print('Scores recorded for ${students[studentIndex - 1]['name']}');
+
         break;
       case '3':
         break;
